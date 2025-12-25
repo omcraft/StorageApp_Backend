276bd6d02a9e33ac0f90fc4d1fb16a6b3d85ba57
omprakashpradhan2023 <omprakashpr2022@gmail.com>
2025-12-25 18:44:49 +0530

    fix 9

--- services/s3.js
+++ services/s3.js
@@ -8,7 +8,9 @@ import {
 } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
-export const s3Client = new S3Client()
+export const s3Client = new S3Client({
+   region: "ap-south-1"
+})
 
 export const createUploadSignedUrl = async ({ key, contentType }) => {
   const command = new PutObjectCommand({
