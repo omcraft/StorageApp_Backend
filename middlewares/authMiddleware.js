0f121a58adba809086756f7d93a6957cede31992
omprakashpradhan2023 <omprakashpr2022@gmail.com>
2025-12-25 18:30:52 +0530

    fix 8

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -4,16 +4,14 @@ export default async function checkAuth(req, res, next) {
   const { sid } = req.signedCookies;
 
   if (!sid) {
-    res.clearCookie("sid");
-    return res.status(401).json({ error: "1 Not logged in!" });
-  }
+  return res.status(401).json({ error: "Not logged in!" });
+}
 
   const session = await redisClient.json.get(`session:${sid}`);
 
   if (!session) {
-    res.clearCookie("sid");
-    return res.status(401).json({ error: "2 Not logged in!" });
-  }
+  return res.status(401).json({ error: "Session expired" });
+}
 
   req.user = { _id: session.userId, rootDirId: session.rootDirId };
   next();
